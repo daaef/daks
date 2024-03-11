ad2feb0173822d612d78bca22ed4d1d6b75eba70
Afekhide <realafe21@gmail.com>
2024-03-11 20:56:34 +0100

    first commit

---
+++ components/headers/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <!-- Add this line for debugging -->
+    <Header1Vue :singlePage="singlePage" v-if="header === 1" />
+    <Header2Vue :singlePage="singlePage" v-else-if="header === 2" />
+    <Header3Vue :singlePage="singlePage" v-else-if="header === 3" />
+    <DefaultHeader :singlePage="singlePage" v-else />
+  </div>
+</template>
+
+<script>
+import { ygencyUtilits } from "~/utilits";
+import DefaultHeader from "./DefaultHeader.vue";
+import Header1Vue from "./Header1.vue";
+import Header2Vue from "./Header2.vue";
+import Header3Vue from "./Header3.vue";
+export default {
+  name: "Header",
+  components: {
+    Header1Vue,
+    Header3Vue,
+    Header2Vue,
+    DefaultHeader,
+  },
+  props: {
+    header: {
+      type: Number,
+      default: 0,
+    },
+    singlePage: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  mounted() {
+    ygencyUtilits.stickyNav();
+  },
+};
+</script>
